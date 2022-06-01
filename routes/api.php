d30cbfeee5e483e6f65195b91982ae9ff4a83f97
Mamagase <gn.mamagase@gmail.com>
2022-06-01 19:44:54 +0200

    Updating Files

---
+++ routes/api.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//public Routes 
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+//Protected Routes 
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    //user
+    Route::get('/user',[AuthController::class,'user']);
+    Route::put('/user',[AuthController::class,'update']);
+    Route::post('/logout',[AuthController::class,'logout']);
+
+    //Post
+    Route::get('/posts', [PostController::class, 'index']); // all posts
+    Route::post('/posts', [PostController::class, 'store']); // create post
+    Route::get('/posts/{id}', [PostController::class, 'show']); // get single post
+    Route::put('/posts/{id}', [PostController::class, 'update']); // update post
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']); // delete post 
+
+    //Comment
+    Route::get('/posts/{id}/comments', [CommentController::class, 'index']); // all comments 
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store']); // create comment on post
+    Route::put('/comments/{id}', [CommentController::class, 'update']); // update comment 
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']); // delete comment 
+
+    //Like
+    Route::post('/posts/{id}/likes', [LikeController::class, 'likeOrUnlike']); // like or dislike back post 
+}); 
